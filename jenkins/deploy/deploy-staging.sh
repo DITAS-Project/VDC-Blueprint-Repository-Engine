0139eb2b41853af8d7668401eec39cc4cd2b812d
Borja Tornos <btornos@ideko.es>
2018-04-24 16:30:14 +0200

    deploy-staging script added

---
+++ jenkins/deploy/deploy-staging.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Staging environment: 31.171.247.162
+# Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem
+
+# TODO state management? We are killing without careing about any operation the conainer could be doing.
+
+ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
+# Ensure that a previously running instance is stopped (-f stops and removes in a single step)
+# || true - "docker stop" failt with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
+sudo docker rm -f VDC-Blueprint-Repository-Engine || true
+sudo docker pull ditas/VDC-Blueprint-Repository-Engine:latest
+sudo docker run -p XXXX:XXXX -d --name VDC-Blueprint-Repository-Engine ditas/VDC-Blueprint-Repository-Engine:latest
+ENDSSH
