1e9395b118a1ea82d00980ff56b473b76a522b13
achilleasmarinakis <37070526+achilleasmarinakis@users.noreply.github.com>
2019-01-24 16:12:35 +0200

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -5,18 +5,12 @@ var after = hooks.after;
 var responseStash = {};
 
 after("/blueprints > POST > 201 > application/json", function (transaction) {
-
-  // saving HTTP response to the stash
   responseStash[transaction.name] = transaction.real.body;
-  console.log(responseStash[transaction.name]);
 });
 
 
 before("/blueprints/{blueprint_id} > DELETE > 204", function (transaction) {
-  //reusing data from previous response here
   var blueprintId = JSON.parse(responseStash['/blueprints > POST > 201 > application/json'])['blueprint_id'];
-
-  //replacing id in URL with stashed id from previous response
   var url = transaction.fullPath;
   transaction.fullPath = url.replace('5c49a32a763d081d264b20e0', blueprintId);
 });
