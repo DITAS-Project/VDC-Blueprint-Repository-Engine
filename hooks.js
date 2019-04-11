efe14f82568d78a597a8db39a05d696befba2f35
achilleasmarinakis <achmarin@mail.ntua.gr>
2019-04-11 14:14:44 +0300

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -8,7 +8,7 @@ after("/blueprints > POST > 201 > application/json", function (transaction) {
   responseStash[transaction.name] = transaction.real.body;
 });
 
-before("/blueprints/{blueprint_id} > GET > 200", function (transaction) {
+before("/blueprints/{blueprint_id} > GET > 200 > application/json", function (transaction) {
   var blueprintId = JSON.parse(responseStash['/blueprints > POST > 201 > application/json'])['blueprint_id'];
   var url = transaction.fullPath;
   transaction.fullPath = url.replace('5c49caf3763d081d264b2637', blueprintId);
