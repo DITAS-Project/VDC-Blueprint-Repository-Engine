387f8c64af675d8320ca9a68b857230dbc156c25
achilleasmarinakis <achmarin@mail.ntua.gr>
2019-10-07 14:41:50 +0300

    Update ElasticInsertionChecker.java

--- src/main/java/gr/ntua/ditas/repositoryengine/restheartcustomization/ElasticInsertionChecker.java
+++ src/main/java/gr/ntua/ditas/repositoryengine/restheartcustomization/ElasticInsertionChecker.java
@@ -121,7 +121,7 @@ public class ElasticInsertionChecker implements Checker {
 			}        
 		}
                 
-		Double tagsFactor = new Double(methodTagWordsArray.size());
+		Double tagsFactor = new Double(tagsArrayList.size());
 		tagsFactor = 1/tagsFactor;
 		tempObj.put("descriptionFactor",descriptionFactor);
 		tempObj.put("tagsFactor",tagsFactor);
